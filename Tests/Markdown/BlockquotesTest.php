82a7a0b238ab4e9fcbbde6bc313745575c4f7821
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-04-20 13:27:02 +0200

    added some markdown inside blockquotes

--- Tests/Markdown/BlockquotesTest.php
+++ Tests/Markdown/BlockquotesTest.php
@@ -65,4 +65,63 @@ this is a second line</p>
 </blockquote>'."\n";
 		$this->assertEquals($expected, $markdown);
 	}
+
+		public function testLazyBlockquoteOnMultipleLine()
+	{
+		$md = $this->getMarkdown();
+		$text = '> hello
+this is a second line
+and another line';
+		$markdown = $md->render($text);
+		$expected = '<blockquote>
+<p>hello<br/>
+this is a second line<br/>
+and another line</p>
+</blockquote>'."\n";
+		$this->assertEquals($expected, $markdown);
+	}
+
+	public function testNestedBlockquote()
+	{
+		$md = $this->getMarkdown();
+		$text = '> hello
+> this is a second line
+>
+> > nested blockquote
+> > second nested line
+>
+> and another line';
+		$markdown = $md->render($text);
+		$expected = '<blockquote>
+<p>hello<br/>
+this is a second line</p>
+<blockquote>
+<p>nested blockquote<br/>
+second nested line</p>
+</blockquote>
+<p>and another line</p>
+</blockquote>'."\n";
+		$this->assertEquals($expected, $markdown);
+	}
+
+	public function testBlockquoteContainingMarkdown()
+	{
+		$md = $this->getMarkdown();
+		$text = '> hello
+> this is a second line
+> ## level2
+> second nested line
+> and another line
+> `<?php echo "hello"; ?>`';
+		$markdown = $md->render($text);
+		$expected = '<blockquote>
+<p>hello<br/>
+this is a second line</p>
+<h2>level2</h2>
+<p>second nested line<br/>
+and another line<br/>
+<code>&lt;?php echo &quot;hello&quot;; ?&gt;</code></p>
+</blockquote>'."\n";
+		$this->assertEquals($expected, $markdown);
+	}
 }
